cde6f65fb444baa44d69d496f66060dfae7c0b9b
Dwayne Bent <dbb@dbb.io>
2013-06-23 11:16:11 -0400

    Add Aggregated.Web.Tests project
    Tests for Aggregated.Web project.

--- Source/Aggregated.Web/Properties/AssemblyInfo.cs
+++ Source/Aggregated.Web/Properties/AssemblyInfo.cs
@@ -1,7 +1,10 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Aggregated.Web")]
 [assembly: AssemblyDescription("Aggregated Web Frontend")]
 
 [assembly: Guid("b366dbad-bd2b-402c-a0c2-fec8424860aa")]
+
+[assembly: InternalsVisibleTo("Aggregated.Web.Tests")]
