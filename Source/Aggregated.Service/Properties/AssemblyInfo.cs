bf017407ec018b38e15f71c06b59ed925fbda1f9
Dwayne Bent <dbb@dbb.io>
2013-06-23 11:47:30 -0400

    Add Aggregated.Service project
    Add Service project for the backend aggregation service.

---
+++ Source/Aggregated.Service/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Aggregated.Service")]
+[assembly: AssemblyDescription("Aggregated Backend Service")]
+
+[assembly: Guid("0f867be2-3541-4bc8-9ab7-e624dd6f4736")]
