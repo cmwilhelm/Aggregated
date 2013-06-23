e25d402e911274811e19763d51d5285f04547220
Dwayne Bent <dbb@dbb.io>
2013-06-23 11:48:20 -0400

    Add Aggregated.Web project
    Add ASP.NET MVC4 project for web frontend.

---
+++ Source/Aggregated.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Aggregated.Web")]
+[assembly: AssemblyDescription("Aggregated Web Frontend")]
+
+[assembly: Guid("b366dbad-bd2b-402c-a0c2-fec8424860aa")]
