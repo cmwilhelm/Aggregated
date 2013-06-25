01d1884f31636af898a5666ffdef42883a11433b
Dwayne Bent <dbb@dbb.io>
2013-06-24 20:30:36 -0400

    Add basic service class
    Runs as a normal console program if started in interactive mode, otherwise
    runs as a service. When in interactive mode, requires that the user press
    Ctrl+C to stop the service.

---
+++ Source/Aggregated.Service/Service.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ServiceProcess;
+using System.Threading;
+
+namespace Aggregated.Service
+{
+    internal class Service : ServiceBase
+    {
+        private static readonly ManualResetEventSlim WaitHandle = new ManualResetEventSlim();
+
+        private static void Main(string[] args)
+        {
+            Console.CancelKeyPress += (s, e) => WaitHandle.Set();
+
+            var service = new Service();
+
+            if (Environment.UserInteractive)
+            {
+                service.OnStart(args);
+
+                WaitHandle.Wait();
+
+                service.OnStop();
+            }
+            else
+            {
+                Run(service);
+            }
+        }
+    }
+}
