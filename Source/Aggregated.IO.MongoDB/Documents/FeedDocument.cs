7cacc770ab46573d39eefa9d06650f33e99c1e5e
Dwayne Bent <dbb@dbb.io>
2013-06-29 13:31:35 -0400

    Add FeedDocument
    A MongoDB IDocument object to transfer FeedModel objects to and from
    MongoDB.

---
+++ Source/Aggregated.IO.MongoDB/Documents/FeedDocument.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson;
+
+namespace Aggregated.IO.MongoDB.Documents
+{
+    public sealed class FeedDocument : IDocument
+    {
+        public ObjectId Id { get; set; }
+        public string Uri { get; set; }
+        public string Name { get; set; }
+    }
+}
