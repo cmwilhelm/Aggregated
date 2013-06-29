098483c3ce408a2c7bb05abd855c6b2b0283d49d
Dwayne Bent <dbb@dbb.io>
2013-06-29 11:22:39 -0400

    Add Aggregated.Model project
    For domain model objects.

---
+++ Source/Aggregated.Model/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Aggregated.Model")]
+[assembly: AssemblyDescription("Aggregated data model")]
+
+[assembly: Guid("2c662478-7b96-44b1-b3d6-a6302fd96dd0")]
