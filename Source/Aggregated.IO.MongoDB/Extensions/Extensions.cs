538d3cb984156bbe0d76eb7b1af4810e06055553
Dwayne Bent <dbb@dbb.io>
2013-06-29 14:58:56 -0400

    Add MongoDB Extensions
    Add some useful extension methods for working with MongoDB.

---
+++ Source/Aggregated.IO.MongoDB/Extensions/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+
+namespace Aggregated.IO.MongoDB.Extensions
+{
+    internal static class Extensions
+    {
+        public static IMongoQuery AsQuery(this IEnumerable<ObjectId> source)
+        {
+            return Query.In("_id", source.Select(i => (BsonValue)i));
+        }
+    }
+}
