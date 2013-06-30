9b3f4f97c744dd1a771e4328549a994356f39e14
Dwayne Bent <dbb@dbb.io>
2013-06-30 12:38:39 -0400

    Add SnapshotDocument
    MongoDB IDocument to store and retrieve SnapshotModels.

---
+++ Source/Aggregated.IO.MongoDB/Documents/SnapshotDocument.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MongoDB.Bson;
+
+namespace Aggregated.IO.MongoDB.Documents
+{
+    public sealed class SnapshotDocument : IDocument
+    {
+        public ObjectId Id { get; set; }
+        public ObjectId FeedId { get; set; }
+        public DateTime RetrievedUtc { get; set; }
+        public string ContentType { get; set; }
+        public string Content { get; set; }
+    }
+}
