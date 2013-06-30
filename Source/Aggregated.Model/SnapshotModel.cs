ef9ead71cae7897085ba58ebd01532d216078c04
Dwayne Bent <dbb@dbb.io>
2013-06-30 12:32:54 -0400

    Add SnapshotModel
    Represents content of a feed as retrieved at a particular time.

---
+++ Source/Aggregated.Model/SnapshotModel.cs
@@ -0,0 +1,27 @@
+﻿using NodaTime;
+
+namespace Aggregated.Model
+{
+    public sealed class SnapshotModel : IModel
+    {
+        public string Id { get; private set; }
+        public string FeedId { get; private set; }
+        public Instant Retrieved { get; private set; }
+        public string ContentType { get; private set; }
+        public string Content { get; private set; }
+
+        public SnapshotModel(string feedId, Instant retrieved, string contentType, string content)
+        {
+            this.FeedId = feedId;
+            this.Retrieved = retrieved;
+            this.ContentType = contentType;
+            this.Content = content;
+        }
+
+        public SnapshotModel(string id, string feedId, Instant retrieved, string contentType, string content)
+            : this(feedId, retrieved, contentType, content)
+        {
+            this.Id = id;
+        }
+    }
+}
