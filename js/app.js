820aa6448fff619d7ab336f5346df2a47b365d60
Chris Wilhelm <chris.wilhelm@gmail.com>
2013-07-04 16:02:44 -0700

    File rename -- scope of the file is an individual binding handler, so rename accordingly.

--- js/app.js
+++ js/app.js
@@ -4,11 +4,12 @@ requirejs.config({
   paths: {
     jquery: ["http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min", "jquery"],
     app: '../app',
-    viewModels: '../app/viewModels'
+    viewModels: '../app/viewModels',
+    bindingHandlers: '../app/bindingHandlers'
   }
 });
 
-require(["jquery", "knockout", "viewModels/AggregatedViewModel", "app/bindingHandlers"],
+require(["jquery", "knockout", "viewModels/AggregatedViewModel"],
   function ($, ko, AggregatedViewModel) {
     /* INIT */
     ko.applyBindings(AggregatedViewModel);
